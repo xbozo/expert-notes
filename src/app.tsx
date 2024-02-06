c963481c5d9cb846087d37b33bec805407dd3c97
Guilherme Viana <imluizguilara@hotmail.com>
2024-02-06 18:08:07 -0300

    feat: add dialogs to note cards

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
 export function App() {
@@ -20,19 +21,9 @@ export function App() {
 			<div className='h-px bg-slate-700' />
 
 			<div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
-				<div className='rounded-md p-5 bg-slate-700 space-y-3'>
-					<span className='text-sm font-medium text-slate-200'>Adicionar nota</span>
+				<NewNoteCard />
 
-					<p className='text-sm leading-6 text-slate-400'>
-						Grave uma nota em áudio que será convertida para texto automaticamente.
-					</p>
-				</div>
-
-				<NoteCard />
-				<NoteCard />
-				<NoteCard />
-				<NoteCard />
-				<NoteCard />
+				<NoteCard note={{ date: new Date(), content: 'Test2e' }} />
 			</div>
 		</main>
 	)
